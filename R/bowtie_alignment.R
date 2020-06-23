45edf199917458edb147bf448c8f981f30a0e950
rpolicastro <rpolicas@indiana.edu>
2020-06-22 21:37:45 -0400

    working on bowtie2 alignment of reads

--- R/bowtie_alignment.R
+++ R/bowtie_alignment.R
@@ -25,7 +25,8 @@ bowtie2_index <- function(
     "bowtie2-build",
     "-f", genome_assembly,
     "--threads", zent_obj@settings[paremter == "ncores", value],
-    str_c(outdir, index_name)
+    str_c(outdir, index_name),
+    sep = " "
   )
 
   ## Run the command.
@@ -44,5 +45,81 @@ bowtie2_index <- function(
   ## Return the zent object.
   return(zent_obj)
 
+}
+
+#' Bowtie2 Alignment
+#'
+#' @importFrom purrr walk map imap
+#'
+#' @param zent_obj Zent object.
+#' @param outdir Output directory for aligned reads.
+#' @param alignment_mode Either 'end-to-end' or 'local'.
+#' @param min_fragment Minimum fragment length (paired end).
+#' @param max_fragment Maximum fragment length (paired end).
+#'
+#' @export
+
+bowtie2_align <- function(
+  zent_obj,
+  outdir = getwd(),
+  alignment_mode = "end-to-end",
+  min_fragment = NA,
+  max_fragment = NA
+) {
+
+  ## Input checks.
+  if (!str_detect(outdir, "/$")) outdir <- str_c(outdir, "/")
+
+  ## Create output directory if it exists.
+  if (!dir.exists(outdir)) dir.create(outdir, recursive = TRUE)
+
+  ## Prepare bowtie2 alignment command.
+  paired_status <- as.logical(zent_obj@settings[parameter == "paired", value])
+
+  commands <- pmap(zent_obj@sample_sheet, function(...) {
+    args <- list(...)
+
+    command <- str_c(
+      "bowtie2",
+      "-x", zent_obj@settings[paramemter == "genome_dir", value],
+      "-S", str_c(outdir, args$sample_name, ".sam"),
+      "--phred33",
+      "--no-unal",
+      "-p", zent_obj@settings[parameter == "ncores", value],
+      sep = " "
+    )
+
+    if (paired_status) {
+      command <- str_c(
+        command,
+        "-1", args$file_1,
+        "-2", args$file_2,
+        "--no-mixed",
+        "--no-discordant",
+        sep = " "
+      )
+
+      if (!is.na(min_fragment)) {
+        command <- str_c(command, "-I", min_fragment, sep = " ")
+      }
+      if (!is.na(max_fragment)) {
+        command <- str_c(command, "-X", max_fragment, sep = " ")
+      }
+    } else {
+      command <- str_c(command, "-U", args$file_1, sep = " ")
+    }
+
+    return(command)
+  })
+
+  ## Run commands.
+  walk(commands, function(x) {
+    system(x, ignore.stdout = TRUE, ignore.stderr = TRUE)
+  })
+
+  ## Make coordinate sorted and indexed bams.
+
+  ## Return the zent object.
+  return(zent_obj)
 
 }
