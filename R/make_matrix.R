b24ce01809ac672601738b6291db3d4597e0f736
Bankso <49208907+Bankso@users.noreply.github.com>
2021-01-07 17:10:32 -0800

    Update make_matrix.R

--- R/make_matrix.R
+++ R/make_matrix.R
@@ -1,14 +1,15 @@
 make_matrix <- function(
   zent_obj,
   outdir = getwd(),
+  bw = bw_in,
+  bed = bed_in,
   bin_size = 1,
   ref_point = 'center',
   min_fragment = 10,
   max_fragment = 700,
   extend_reads = 200,
-  before_field = 1000
-  after_field = 1000
-  scale_factors = NA,
+  before_field = 1000,
+  after_field = 1000,
   temp_dir = "./temp"
 ) {
 
@@ -20,46 +21,18 @@ make_matrix <- function(
   ## Make output directory if it doesn't exist.
   if (!dir.exists(outdir)) dir.create(outdir, recursive = TRUE)
 
-  ## Get bams.
-  if (analysis_type %in% c("ChIP-seq", "ChEC-seq")) {
-    samples <- split(
-      zent_obj@sample_sheet[, .(sample_name, sample_bams)],
-      by = "sample_name",
-      keep.by = FALSE
-    )
-    samples <- map(samples, as.character)
-
-    if(any(!is.na(zent_obj@sample_sheet[["control_bams"]]))) {
-      controls <- split(
-        unique(zent_obj@sample_sheet[
-          !is.na(control_bams),
-          .(control_name, control_bams)
-        ]),
-        by = "control_name",
-        keep.by = FALSE
-      )
-      controls <- map(controls, as.character)
-      samples <- c(samples, controls)
-    }
-  } else {
-    samples <- split(
-      zent_obj@sample_sheet[, .(sample_name, bam_files)],
-      by = "sample_name",
-      keep.by = FALSE
-    )
-    samples <- map(samples, as.character)
-  }
-
   ## Prepare command.
   commands <- imap(samples, function(x, y) {
     command <- str_c(
       "computeMatrix",
-      "-R", x,
+	  "reference-point",
+      "-R", bed,
       "-S", bw,
-      "-o", "matrix"
+	  "--referencePoint", ref_point,
+      "-o", "matrix",
       "-bs", bin_size,
-      "-b", before_field
-      "-a", after_field
+      "-b", before_field,
+      "-a", after_field,
       "-p", pull_setting(zent_obj, "ncores"),
       sep = " "
     )
